dbc146670b637e2c5d794c918e2108fec065a199
Steven Crawford <scrawford@redstonecontentsolutions.com>
2020-10-03 18:58:30 -0500

    Added login form endpoint

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.get('/', (request, response) => {
     return response.send("<h1>HELLO API</h1>");
 });
 
+app.get('/login', (req, res) => {
+    return res.send(`
+    <form>
+        <lable for=username>username</lable>
+        <input id=username type=text></input>
+        <lable for=password>password</lable>
+        <input id=password type=password></input>
+        <input type=submit>Login!</input>
+    </form>
+    `);
+
+});
+
+
 app.get('/users/:username', (req, res) => {
     return res.send({'username' : req.params.username});
 });
